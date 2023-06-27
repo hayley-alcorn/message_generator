ad367aa0d7d1a380199ede6b1e6937546073c128
Hayley Alcorn <hayleyjalcorn@aol.com>
2023-06-27 16:40:56 +0100

    Additional quote added

--- messageGenerator.js
+++ messageGenerator.js
@@ -2,7 +2,8 @@ const quotes = ['Just keep swimming.',
 "You've got a friend in me", 
 'Some people are worth melting for.', 
 'Any day spent with you is my favorite day. So, today is my new favorite day.', 
-'The past can hurt. But the way I see it, you can either run from it, or learn from it.'];
+'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
+"You're mad. Bonkers. Off your head ... But i'll tell you a secret ... some of the best people are."];
 
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
