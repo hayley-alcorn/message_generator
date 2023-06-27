e9df4f5b2ee7291ef8021f5d057e51411fbd94ce
Hayley Alcorn <hayleyjalcorn@aol.com>
2023-06-27 17:05:05 +0100

    Switch statement added. Results in three quotes outputted

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,17 +1,34 @@
-const quotes = ['Just keep swimming.', 
-"You've got a friend in me", 
-'Some people are worth melting for.', 
-'Any day spent with you is my favorite day. So, today is my new favorite day.', 
-'The past can hurt. But the way I see it, you can either run from it, or learn from it.',
-"You're mad. Bonkers. Off your head ... But i'll tell you a secret ... some of the best people are."];
+const quotes = {
+    'love quotes':['Some people are worth melting for. - Olaf', 'Love is a song that never ends. - Bambi', "My dream wouldn't be complete without you in it. - Tiana"],
+    'friendship quotes': ['Just keep swimming. - Dory', "You've got a friend in me. - Toy Story", 'Any day spent with you is my favorite day. So, today is my new favorite day - Winnie the Pooh.'],
+    'inspirational quotes': ['The past can hurt. But the way I see it, you can either run from it, or learn from it. - Rafiki', "You're mad. Bonkers. Off your head ... But i'll tell you a secret ... some of the best people are. - Alice",'Life is a journey to be experienced, not a problem to be solved. - Winnie the Pooh']
+};
+
+let resultQuote = [];
 
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
 }
 
 function generateRandomQuote(quote) {
-  let newNum = generateRandomNumber(quotes.length)
-  console.log(quotes[newNum])
+  for (let disneyquote in quotes) {
+    let newNum = generateRandomNumber(quote[disneyquote].length)
+
+    switch(disneyquote) {
+        case 'love quotes':
+            resultQuote.push(quote[disneyquote][newNum])
+            break
+        case 'friendship quotes':
+            resultQuote.push(quote[disneyquote][newNum])
+            break
+        case 'inspirational quotes':
+            resultQuote.push(quote[disneyquote][newNum])
+            break
+        default:
+            resultQuote.push("Anything is possible.")
+    }
+  }
+  console.log(resultQuote);
 }
 
-generateRandomQuote(quotes)
+generateRandomQuote(quotes);
